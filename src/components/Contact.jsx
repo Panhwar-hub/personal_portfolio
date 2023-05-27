b8d5d8a823c3db473093ef14023e91d42034bf07
Parvez Ali <pervzpanh786@gmail.com>
2023-05-27 15:32:45 +0500

    deploying page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -7,6 +7,10 @@ import { EarthCanvas } from "./canvas";
 import { SectionWrapper } from "../hoc";
 import { slideIn } from "../utils/motion";
 
+// template_ityq33r
+//service_l4ka0ii
+//WHJ5UjwKedaCd1uCn
+
 const Contact = () => {
   const formRef = useRef();
   const [form, setForm] = useState({
@@ -33,8 +37,8 @@ const Contact = () => {
 
     emailjs
       .send(
-        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        "service_l4ka0ii",
+        "template_ityq33r",
         {
           from_name: form.name,
           to_name: "Parvez Ali",
@@ -42,7 +46,7 @@ const Contact = () => {
           to_email: "pervzpanh786@gmail.com",
           message: form.message,
         },
-        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+        "WHJ5UjwKedaCd1uCn"
       )
       .then(
         () => {
